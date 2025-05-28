841d061f33c35c366f4d7bfb25591f7d5eff0463
mbarros46 <miguelbarrosramos46@gmail.com>
2025-05-28 18:38:03 -0300

    ajustes

--- src/main/java/br/com/fiap/stormsafe/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/stormsafe/repository/UsuarioRepository.java
@@ -1,11 +1,11 @@
-
 package br.com.fiap.stormsafe.repository;
 
 import br.com.fiap.stormsafe.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    Optional<Usuario> findByEmail(String email);
 }
